1e0bc379dd18bed82c38c9f4e9883dfb4cb4d546
Nandini1857 <choudharynandini765@gmail.com>
2025-07-28 19:06:00 +0530

    Create bio_generator.py

---
+++ bio_generator.py
@@ -0,0 +1,7 @@
+name = input("What's your name? ")
+age = input("How old are you? ")
+color = input("What's your favorite color? ")
+
+print("\n--- Your Cool Bio ---")
+print(f"Hi, my name is {name}.")
+print(f"I'm {age} years old and I love the color {color}! 🎨")
